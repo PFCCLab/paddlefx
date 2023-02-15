fe6ef4d48f600c40b9c84a0afc590f25a266d8a8
Allen Guo <guosonglin001@gmail.com>
2023-02-15 14:47:37 +0000

    add basic project tree

---
+++ src/paddlefx/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from .version import version as __version__
+    from .version import version_tuple
+except ImportError:
+    __version__ = version = '0.0.0.unknown'
+    __version_tuple__ = version_tuple = (0, 0, 0, "unknown")
