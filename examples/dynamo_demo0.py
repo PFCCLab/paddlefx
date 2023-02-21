40eb65184d55055b7c70b544c2ef687c79c1f1fa
Allen Guo <guosonglin001@gmail.com>
2023-02-21 17:01:03 +0000

    wip

--- examples/dynamo_demo0.py
+++ examples/dynamo_demo0.py
@@ -20,6 +20,12 @@ print('\nstart set_eval_frame(None)')
 set_eval_frame(None)
 print('fin set_eval_frame(None)')
 
+add(1, 4)
+
+print('\nstart set_eval_frame(callback)')
+set_eval_frame(callback)
+print('fin set_eval_frame(callback)')
+
 add(1, 3)
 
 print('fin')
