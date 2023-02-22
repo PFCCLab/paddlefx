41dcd9d315a8e00ffb1dc346268de6a9d39cd86c
Allen Guo <guosonglin001@gmail.com>
2023-02-22 03:47:00 +0000

    wip

--- src/paddlefx/__init__.py
+++ src/paddlefx/__init__.py
@@ -6,7 +6,7 @@ except ImportError:
     __version_tuple__ = version_tuple = (0, 0, 0, "unknown")
 
 
-# from .graph import Graph
-# from .node import Node
-# from .proxy import Proxy
-# from .symbolic_trace import Tracer, symbolic_trace
+from .graph import Graph
+from .node import Node
+from .proxy import Proxy
+from .symbolic_trace import Tracer, symbolic_trace
