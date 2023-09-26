4089fa8d224df16adc3f30bd543d497082ec3876
Zhan Rongrui <2742392377@qq.com>
2023-09-26 23:02:06 +0800

    improve example

--- examples/simple_compiler.py
+++ examples/simple_compiler.py
@@ -23,8 +23,5 @@ optimized_net = paddlefx.optimize(CompilerBase(print_tabular=True))(add)
 x = paddle.rand([1, 224])
 out = add(x, x)
 res = optimized_net(x, x)
-res = optimized_net(x, x)
-res = optimized_net(x, x)
-
 
 np.testing.assert_equal(res.numpy(), out.numpy())
