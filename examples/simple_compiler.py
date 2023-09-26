1c9e810fd09dd7dd91a774231c3b47db7a072664
Zhan Rongrui <2742392377@qq.com>
2023-09-26 21:49:10 +0800

    update example

--- examples/simple_compiler.py
+++ examples/simple_compiler.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import logging
+
 import numpy as np
 import paddle
 import paddle.nn
@@ -7,17 +9,22 @@ import paddle.tensor
 
 import paddlefx
 
+from paddlefx.compiler import CompilerBase
+
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 def add(x, y):
     return x + y
 
 
-optimized_net = paddlefx.optimize()(add)
+optimized_net = paddlefx.optimize(CompilerBase(print_tabular=True))(add)
 
 x = paddle.rand([1, 224])
 out = add(x, x)
 res = optimized_net(x, x)
 res = optimized_net(x, x)
 res = optimized_net(x, x)
+
 
 np.testing.assert_equal(res.numpy(), out.numpy())
