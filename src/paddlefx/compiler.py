78d3dcb184997157a6de23b501920116bacdf2f3
Zhan Rongrui <2742392377@qq.com>
2023-09-27 13:32:31 +0800

    update compiler

--- src/paddlefx/compiler.py
+++ src/paddlefx/compiler.py
@@ -15,8 +15,17 @@ class CompilerBase:
         return self.compile(gl, inputs)
 
     def compile(self, gl: paddlefx.GraphLayer, inputs: list | tuple) -> Callable:
-        for node in gl.graph.nodes:
-            # TODO(zrr1999): support more ops
-            if node.op == "output":
-                pass
-        return gl.forward
+        try:
+            for node in gl.graph.nodes:
+                getattr(self, f"compile_{node.op}")(node)
+            return gl.forward  # TODO(zrr1999): return compiled_func
+        except NotImplementedError:
+            print("AttributeError when compiling graph")
+            return gl.forward
+
+    def compile_output(self, node: paddlefx.Node):
+        raise NotImplementedError
+
+
+class NumpyCompiler(CompilerBase):
+    pass
