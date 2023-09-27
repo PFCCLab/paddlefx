d30a921ae62a9d9a8156ab7f32a262fa6be7292e
Zhan Rongrui <2742392377@qq.com>
2023-09-27 13:37:37 +0800

    fix bug

--- src/paddlefx/compiler.py
+++ src/paddlefx/compiler.py
@@ -19,9 +19,12 @@ class CompilerBase:
             for node in gl.graph.nodes:
                 getattr(self, f"compile_{node.op}")(node)
             return gl.forward  # TODO(zrr1999): return compiled_func
-        except NotImplementedError:
+        except (NotImplementedError, AttributeError):
             print("AttributeError when compiling graph")
             return gl.forward
+
+    def compile_placeholder(self, node: paddlefx.Node):
+        raise NotImplementedError
 
     def compile_output(self, node: paddlefx.Node):
         raise NotImplementedError
