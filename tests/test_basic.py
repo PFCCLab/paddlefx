2e6ae7047eb5c1ba9618c35e6d596aad8a0643b6
Zhan Rongrui <2742392377@qq.com>
2023-09-28 11:29:02 +0800

    add test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,8 +6,10 @@ import paddle.nn
 
 import paddlefx
 
+from paddlefx.compiler import TVMCompiler
 
-@paddlefx.optimize()
+
+@paddlefx.optimize(backend=TVMCompiler())
 def add(x, y):
     z = x + y
     return z
@@ -18,19 +20,19 @@ def func(x, y):
     return z
 
 
-@paddlefx.optimize()
+@paddlefx.optimize(backend=TVMCompiler())
 def func_add(a, b):
     d = func(a, b)
     return d
 
 
 def test_add():
-    in_a = paddle.rand([1])
-    in_b = paddle.rand([1])
+    in_a = paddle.rand([1, 224])
+    in_b = paddle.rand([1, 224])
     np.testing.assert_allclose(add(in_a, in_b), in_a + in_b)
 
 
 def test_func_add():
-    in_a = paddle.rand([1])
-    in_b = paddle.rand([1])
+    in_a = paddle.rand([1, 224])
+    in_b = paddle.rand([1, 224])
     np.testing.assert_allclose(func_add(in_a, in_b), in_a + in_b)
