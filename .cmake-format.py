0bc64cceeefdef5a70472ea44f5f5233ce9fc66f
Zhan Rongrui <2742392377@qq.com>
2023-10-14 15:58:57 +0800

    update

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,8 +1,6 @@
 # ------------------------------------------------
 # Options affecting comment reflow and formatting.
 # ------------------------------------------------
-from __future__ import annotations
-
 with section("markup"):
     # enable comment markup parsing and reflow
     enable_markup = False
